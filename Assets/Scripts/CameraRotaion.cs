09fecb868591e27968ea9810290322346ab52323
THERIEZ Kilian <kiliantheriez@gmail.com>
2023-11-30 12:48:33 +0100

    Camera 360 Rotation

---
+++ Assets/Scripts/CameraRotaion.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraRotaion : MonoBehaviour
+{
+    [SerializeField] private Transform _mainCameraPivot;
+    [SerializeField] [Range(10f, 100f)] private float _cameraRotationSpeed; 
+    private Vector2 _rotationAxis;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (_rotationAxis != Vector2.zero)
+        {
+            _mainCameraPivot.localEulerAngles += new Vector3(0, _rotationAxis.x * _cameraRotationSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    public void OnCameraRotation(InputAction.CallbackContext callback)
+    {
+        _rotationAxis = callback.ReadValue<Vector2>();
+    }
+}
