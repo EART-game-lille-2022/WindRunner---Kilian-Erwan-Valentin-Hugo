55c6438bf3ff6649f99ec0ac05a730e381e1a3bf
Batpion <erwan.mounier@hotmail.fr>
2024-03-07 10:28:38 +0100

    Quests scripts from nelow

---
+++ Assets/Scripts/QuestScripts/MissionStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionStart : MonoBehaviour
+{
+    public S_Mission missionStart;
+
+    // IEnumerator Start()
+    // {
+    //     yield return new WaitForSeconds(5);
+    //     missionStart.StartQuest();
+    // }
+
+    public void StartQuest()
+    {
+        missionStart.StartMission();
+    }
+}
