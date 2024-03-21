c7ec6b5ecb993ba45676def70980bc1ee1eac2dc
Batpion <erwan.mounier@hotmail.fr>
2024-03-21 12:35:57 +0100

    Script spawn aleatoire (marche pas encore)

--- Assets/Scripts/Quest/Spawner.cs
+++ Assets/Scripts/Quest/Spawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    //MARCHE PAS
     // [header("Parametrage")]
     // public GameObject POIs;
     // public float spawnChance;
@@ -14,14 +15,27 @@ public class Spawner : MonoBehaviour
     // public LayerMask layerMask;
     // public Vector2 positivePosition, negativePosition;
     // // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // void Start()
+    // {
+    //     SpawnPOIs();
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // // Update is called once per frame
+    // void SpawnPOIs()
+    // {
+    //     for (float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenCheck)
+    //     {
+    //          for (float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenCheck)
+    //     {
+    //         RaycastHit hit;
+    //         if(Physics.Raycast(new Vector 3(x, heightOfCheck, z), Vector3.down, out hit, rangeOfCheck, layerMask))
+    //         {
+    //             if(spawnChance > Random.Range(0, 101))
+    //             {
+    //                 Instantiate(POIs, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), transform);
+    //             }
+    //         }
+    //     }
+    //     }
+    // }
 }
