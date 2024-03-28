a51ad7f2e237d36a595ff53fc5d9a9caee3b0c26
THERIEZ Kilian <kiliantheriez@gmail.com>
2024-03-28 09:57:53 +0100

    Update sail rotation & sailAngle indicator

--- Assets/Scripts/Boat/SailRotation.cs
+++ Assets/Scripts/Boat/SailRotation.cs
@@ -7,8 +7,8 @@ public class SailRotation : MonoBehaviour
 
     public void RotareSail(float value)
     {
-        float angle = value.Remap(0, 1, 0, 180);
-        angle = Mathf.Clamp(angle, 1, 179);
+        float angle = value.Remap(0, 1, 32, 180);
+        angle = Mathf.Clamp(angle, 33, 179);
         currentAngle = angle;
         transform.localEulerAngles = new Vector3(0, currentAngle, 0);
     }
