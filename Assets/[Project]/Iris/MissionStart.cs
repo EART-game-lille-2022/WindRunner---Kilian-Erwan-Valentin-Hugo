10806dcc2b2824c02da82dca8722b122e1bbbac6
Batpion <erwan.mounier@hotmail.fr>
2024-05-02 10:45:41 +0200

    suppression erreur de script (mauvaise branche)

--- Assets/[Project]/Iris/MissionStart.cs
+++ Assets/[Project]/Iris/MissionStart.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class MissionStart : MonoBehaviour
-{
-    public S_Mission missionStart; //Entré scriptable object de la mission a demmarer
+// public class MissionStart : MonoBehaviour
+// {
+//     public S_Mission missionStart; //Entré scriptable object de la mission a demmarer
 
-     IEnumerator Start()        
-     {
-         yield return new WaitForSeconds(1); //Attendre 1s
-         missionStart.StartMission(); // et lancé la mission entré plus tot
-     }
+//      IEnumerator Start()        
+//      {
+//          yield return new WaitForSeconds(1); //Attendre 1s
+//          missionStart.StartMission(); // et lancé la mission entré plus tot
+//      }
 
-    public void StartQuest()
-    {
-        missionStart.StartMission();
-    }
-}
+//     public void StartQuest()
+//     {
+//         missionStart.StartMission();
+//     }
+// }
--- Assets/[Project]/Iris/S_MissionObjective.cs
+++ Assets/[Project]/Iris/S_MissionObjective.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[CreateAssetMenu(fileName = "Mission_", menuName = "Data/Mission_Objective", order = 1)]
-public class S_MissionObjective : ScriptableObject
-{
-    public virtual bool CheckFinish() {
-        return false;
-    }
-    public virtual void Start() {
-    }
-    public virtual void End() {
-    }
-}
+// [CreateAssetMenu(fileName = "Mission_", menuName = "Data/Mission_Objective", order = 1)]
+// public class S_MissionObjective : ScriptableObject
+// {
+//     public virtual bool CheckFinish() {
+//         return false;
+//     }
+//     public virtual void Start() {
+//     }
+//     public virtual void End() {
+//     }
+// }
