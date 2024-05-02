81fcba7c9e254d591317d82bd9b2bf1d06dc66ff
THERIEZ Kilian <kiliantheriez@gmail.com>
2024-05-02 11:30:47 +0200

    Make wind Generation continious

--- Assets/Scripts/Boat/WindSpawner.cs
+++ Assets/Scripts/Boat/WindSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class WindSpawner : MonoBehaviour
@@ -5,14 +6,32 @@ public class WindSpawner : MonoBehaviour
     public GameObject prefab;
     public Transform refPosRot;
     public Transform _spawnPosition;
+    [SerializeField, Range(0f, 2f)] private float _spawnDelay;
+    private bool _canCreatePoint;
+
+    private void Start()
+    {
+        _canCreatePoint = true;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
+            StartCoroutine(CreateWindPoint());
+        }
+    }
+
+    IEnumerator CreateWindPoint()
+    {
+        if (_canCreatePoint == true) 
+        { 
+            _canCreatePoint = false;
             Vector3 forward = refPosRot.forward;
             forward.y = 0;
             Instantiate(prefab, _spawnPosition.position, Quaternion.LookRotation(forward));
+            yield return new WaitForSeconds(_spawnDelay);
+            _canCreatePoint = true;
         }
     }
 }
