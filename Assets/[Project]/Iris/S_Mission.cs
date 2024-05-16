1098611779e142833b652d33100a0305ad26ab3e
Batpion <erwan.mounier@hotmail.fr>
2024-05-16 09:27:16 +0200

    Comment script

--- Assets/[Project]/Iris/S_Mission.cs
+++ Assets/[Project]/Iris/S_Mission.cs
@@ -1,32 +1,32 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.InputSystem.Interactions;
+//  using System.Collections;
+//  using System.Collections.Generic;
+//  using UnityEngine;
+//  using UnityEngine.InputSystem.Interactions;
 
-// [CreateAssetMenu(fileName = "Mission_", menuName = "Data/Mission", order = 1)]
-// public class S_Mission : ScriptableObject
-// {
-//     public static List<S_Mission> missionsEnCours = new List<S_Mission>();
-//     public string missionName; //Nomer la mission en public
-//     public bool isUnlocked = false; //De base, la mission n'est pas debloqué
-//    // public ScriptableDialogue dialogue;
+//  [CreateAssetMenu(fileName = "Mission_", menuName = "Data/Mission", order = 1)]
+//  public class S_Mission : ScriptableObject
+//  {
+//      public static List<S_Mission> missionsEnCours = new List<S_Mission>();
+//      public string missionName; //Nomer la mission en public
+//      public bool isUnlocked = false; //De base, la mission n'est pas debloqué
+//     // public ScriptableDialogue dialogue;
 
-//     public S_Mission[] unlockedOnFinish; //Quete debloqué lors de complétion de cette quete
+//      public S_Mission[] unlockedOnFinish; //Quete debloqué lors de complétion de cette quete
 
-//     public S_MissionObjective[] objectifList;   //Entré du scriptable object qui comprend les objet a recuperer pour la quete
+//      public S_MissionObjective[] objectifList;   //Entré du scriptable object qui comprend les objet a recuperer pour la quete
 
-//     public void StartMission() {
-//         Debug.Log("Start Mission");
-//   //      DialogueManager.instance.PlayDialogue(dialogue);
-//         foreach(S_MissionObjective objectif in objectifList) {
-//             objectif.Start();
-//         }
-//         if(!missionsEnCours.Contains(this)) missionsEnCours.Add(this);
-//     }
-//     public bool CheckFinish() {
-//         foreach(S_MissionObjective objectif in objectifList) {
-//             if(!objectif.CheckFinish()) return false;
-//         }
-//         return true;
-//     }
-// }
+//      public void StartMission() {
+//          Debug.Log("Start Mission");
+//    //      DialogueManager.instance.PlayDialogue(dialogue);
+//          foreach(S_MissionObjective objectif in objectifList) {
+//              objectif.Start();
+//          }
+//          if(!missionsEnCours.Contains(this)) missionsEnCours.Add(this);
+//      }
+//      public bool CheckFinish() {
+//          foreach(S_MissionObjective objectif in objectifList) {
+//              if(!objectif.CheckFinish()) return false;
+//          }
+//          return true;
+//      }
+//  }
